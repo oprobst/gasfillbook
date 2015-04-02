39fbef1daf82e8fd5b8c8e844b5011af3580374c
oprobst <OliverProbst@gmx.de>
2015-04-02 13:14:59 +0200

    Better demo data.

--- src/test/java/de/tsvmalsch/demo/InitDBWithDemoData.java
+++ src/test/java/de/tsvmalsch/demo/InitDBWithDemoData.java
@@ -12,17 +12,124 @@ import de.tsvmalsch.shared.model.FillingInvoiceItem;
 import de.tsvmalsch.shared.model.Member;
 
 public class InitDBWithDemoData {
+	private static String getFirstName(int i) {
+		if (i > 140) i = 0;
+		final String[] names = { "O ", "Mia ", "Emma ", "Hannah ", "Sofia ",
+				"Anna ", "Lea ", "Emilia ", "Marie ", "Lena ", "Leonie ",
+				"Emily ", "Lina ", "Amelie ", "Sophie ", "Lilly ", "Luisa ",
+				"Johanna ", "Laura ", "Nele ", "Lara ", "Maja ", "Charlotte ",
+				"Clara ", "Leni ", "Sarah ", "Pia ", "Mila ", "Alina ",
+				"Lisa ", "Lotta ", "Ida ", "Julia ", "Greta ", "Melina ",
+				"Paula ", "Marlene ", "Jana ", "Helena ", "Isabella ",
+				"Elisa ", "Amy ", "Mira ", "Katharina ", "Stella ", "Antonia ",
+				"Annika ", "Fiona ", "Pauline ", "Nora ", "Eva ", "Jule ",
+				"Magdalena ", "Luna ", "Merle ", "Maria ", "Nina ", "Melissa ",
+				"Franziska ", "Milena ", "Ronja ", "Elena ", "Romy ", "Mina ",
+				"Helene ", "Selina ", "Paulina ", "Vanessa ", "Fabienne ",
+				"Zoey ", "Miriam ", "Linda ", "Samira ", "Elina ", "Jolina ",
+				"Celina ", "Elisabeth ", "Valentina ", "Julie", "Olivia ",
+				"Jette ", "Kim ", "Elif ", "Lotte ", "Anastasia ", "Luana ",
+				"Rosalie ", "Mona ", "Lana ", "Diana ", "Thea ", "Alexandra ",
+				"Angelina ", "Ben ", "Luca ", "Paul ", "Jonas ", "Finn ",
+				"Leon ", "Luis ", "Lukas ", "Maximilian ", "Felix ", "Noah ",
+				"Elias ", "Julian ", "Max ", "Tim ", "Moritz ", "Henry ",
+				"Niklas ", "Philipp ", "Jakob ", "Tom ", "Jan ", "Emil ",
+				"Alexander ", "David ", "Oskar ", "Fabian ", "Anton ", "Erik ",
+				"Rafael Dean ", "Adam ", "Diego ", "Piet ", "Franz ", "Fritz ",
+				"Hugo ", "Michel", " Ilias Malik", " Nikita ", "Henrik ",
+				"Lionel ", "Martin ", "Clemens Milo ", "Ian", " Len Emilian",
+				" Yusuf", " Lio ", "Jasper", " Lion ", "Maik Eddi Can", " Ali",
+				" Berat ", "Maurice ", "Andreas", " Taylor ", "Kevin",
+				" Alessio ", "Kai Laurens Carlo Leonardo ", "Willi Benno",
+				" Devin ", "Luan ", "Gustav ", "Leonhard ", "Mert", " Chris",
+				" Thore Ryan", " Yasin ", "Fiete ", "Henning", " Arian ",
+				"Roman ", "Korbinian", " Carlos", " Jonte", " Alessandro ",
+				"Peter", " Deniz", " Nino", " Alex", " Antonio", " Mailo",
+				" Brian Amir ", "Christopher", " Thilo Damien", " Mehmet",
+				" Ricardo Dario", " Joris", " Victor Darian ", "Josef Jack",
+				" Kaan", " Mick ", "Enno ", "Kjell", " Aiden Domenic Jesper",
+				" Enes", " Ömer", " Titus", " Hamza", " Mustafa", " Mikail ",
+				"Torben Jaron ", "Ruben", " Bjarne", " Miran ",
+				"Stefan Claas Edgar", " Jerome ", "Leif", " Leonas", " Lino",
+				" Romeo", " Andre ", "Nathan", " Tino", " William ", "Hanno",
+				" Sami ", "Ahmet", " Miguel ", "Steven", " Emin", " Lean",
+				" Mirac", " Semih", " Sinan", " Etienne", " Ibrahim ",
+				"Mario ", "Timon", " Xaver ", "Armin", " Efe", " Janosch",
+				" Kerem", " Mio ", "Wilhelm ", "Albert ", "Erwin ", "Hans",
+				" Marian", " Anthony", " Cem", " Emre", " Eymen", " Leonidas",
+				" Aras", " Ensar ", "Kenan", " Kuzey", " Lutz", " Selim ",
+				"Tamme", " Valentino", " Danny", " Emanuel", " Giuliano",
+				" Hassan Kerim", " Umut", " Amin", " Arda", " Danilo", " Eren",
+				" Mattes", " Vince", " Arvid", " Darius ", "Dustin", " Jake",
+				" Jarne", " Jim ", "Marten", " Sean", " James", " Jean",
+				" Lucien", " Rayan", " Elian", " Emirhan", " Furkan", " Jonne",
+				" Kalle", " Karim", " Milian", " Timur", " Damon ", "Enrico",
+				" Marek", " Quentin", " Alwin", " Angelo", " Jesse ", "Otto",
+				" Samir", " Yassin", " Bilal", " Caspar", " Jannek Jarno",
+				" Maddox", " Mahir", " Marlo", " Rico", " Tjark",
+				" Elija Iven Joscha", " Nicolai Rocco ", "Sven", " Berkay",
+				" Dion", " Gregor", " Jano", " Koray", " Ramon", " Sandro",
+				" Taylan", " Davin", " Francesco", " Jamal", " Jordan",
+				" Lewis", " Omar", " Pius", " Taha", " Veit", " Amar",
+				" Eduard", " Gianluca", " Ismail", " Joost Lucian", " Miko",
+				" Sirac", " Thies ", "Alfred", " Dylan", " Eray", " Flynn",
+				" Hauke", " Logan ", "Melvin", " Younes", " Ilja", " Jon",
+				" Mete", " Tamino", " Alan", " Arjen", " Jenke", " Johnny",
+				" Keno", " Loris", " Milow", " Santino", " Tiago Burak",
+				" Fabrizio", " Gian", " Hennes", " Kirill", " Lorik", " Luiz",
+				" Peer", " Talha", " Tizian", " Tommy", " Yunus", " Aidan",
+				" Baran ", "Björn", " Cornelius", " Dorian ", "Hagen",
+				" Leano", " Quinn", " Youssef", " Benny", " Bent", " Ege",
+				" Gero", " Ivan", " Kimi", " Levent", " Luuk", " Maris",
+				" Miro", " Sören", " Stanley", " Vito", " Yigit", " Batuhan",
+				" Edwin", " Jakub", " Juri", " Kiyan", " Meo", " Musa",
+				" Raul", " Ron", " Rüzgar", " Said", " Sammy", " Santiago",
+				" Severin", " Adem", " Adriano", " Alexandros", " Ansgar ",
+				"August", " Bo", " Dejan", " Eyüp", " Fridolin", " Hüseyin",
+				" Ilay", " Jay", " Jimmy", " Josua", " Merlin", " Nathanael",
+				" Quirin", " Azad", " Davide", " Denny", " Evan", " Giuseppe",
+				" Jari", " Jascha", " Neven", " Pierre", " Rick", " Tarik",
+				" Vinzenz", " Ahmad", " Amon", " Andrej", " Aurel", " Bosse",
+				" Demian", " Ethan", " Joe", " Joey", " Nelio ", "René",
+				" Salvatore" };
+		return names[i].trim();
+	}
+
+	private static String getLastName(int i) {
+		if (i > 97) i = 0;
+		final String[] names = { "Müller", "Schneider", "Fischer", "Weber",
+				"Meyer", "Wagner", "Becker", "Schulz", "Hoffmann", "Schäfer",
+				"Koch", "Bauer", "Richter", "Klein", "Übername", "Schröder",
+				"Neumann", "Schwarz", "Zimmermann", "Braun", "Krüger",
+				"Hofmann", "Hartmann", "Lange", "Schmitt", "Werner", "Schmitz",
+				"Krause", "Meier", "Lehmann", "Schmid", "Schulze", "Maier",
+				"Köhler", "Herrmann", "König", "Walter", "Mayer", "Huber",
+				"Kaiser", "Fuchs", "Peters", "Lang", "Scholz", "Möller",
+				"Weiß", "Jung", "Hahn", "Schubert", "Vogel", "Friedrich",
+				"Keller", "Günther", "Frank", "Berger", "Winkler", "Roth",
+				"Beck", "Lorenz", "Baumann", "Franke", "Albrecht", "Schuster",
+				"Simon", "Ludwig", "Böhm", "Winter", "Kraus", "Martin",
+				"Schumacher", "Krämer", "Vogt", "Stein", "Jäger", "Otto",
+				"Sommer", "Groß", "Seidel", "Heinrich", "Brandt", "Haas",
+				"Schreiber", "Graf", "Schulte", "Dietrich", "Ziegler", "Kuhn",
+				"Kühn", "Pohl", "Engel", "Horn", "Busch", "Bergmann", "Thomas",
+				"Voigt", "Sauer", "Arnold", "Wolff" };
+		return names[i];
+	}
+
 	public static void main(String[] args) {
 		Session session = HibernateUtil.getSessionFactory().getCurrentSession();
 		session.beginTransaction();
 
 		for (int i = 0; i < 100; i++) {
 			Member m = new Member();
-			m.setFirstName("F" + i);
-			m.setLastName("L" + i);
+			m.setFirstName(InitDBWithDemoData.getFirstName(i));
+			m.setLastName(InitDBWithDemoData.getLastName(i));
 			m.setMemberNumber(i);
 			m.setEncodedPassword("1");
-			m.setEmail("me@home.de");
+			m.setEmail(InitDBWithDemoData.getFirstName(i) + "." + InitDBWithDemoData.getLastName(i) +"@tsv-malsch.de");
+			m.setIsActive(true);
+			m.setHasGasblenderBrevet(true);
 			session.save(m);
 
 			Cylinder cy = new Cylinder();
@@ -69,8 +176,6 @@ public class InitDBWithDemoData {
 				session.save(fii);
 			}
 		}
-
-	 
 
 		session.getTransaction().commit();
 		System.out.println("Dumped Test data.");
