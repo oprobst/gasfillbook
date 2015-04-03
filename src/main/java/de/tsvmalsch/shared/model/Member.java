9ff955a745589f804858e71f4f0ded39be63acca
Oliver Probst <oliverprobst@gmx.de>
2015-04-03 11:22:42 +0200

    Added javadoc to member variables.

--- src/main/java/de/tsvmalsch/shared/model/Member.java
+++ src/main/java/de/tsvmalsch/shared/model/Member.java
@@ -16,6 +16,12 @@ import javax.validation.constraints.Size;
 
 import org.hibernate.annotations.GenericGenerator;
 
+/**
+ * This is a data object representing a club member.
+ * 
+ * @author Oliver Probst
+ *
+ */
 @Entity
 public class Member implements Serializable {
 
@@ -24,41 +30,81 @@ public class Member implements Serializable {
 	 */
 	private static final long serialVersionUID = 3113539902485969147L;
 
+	/**
+	 * Cylinders owned by this member
+	 */
 	@OneToMany(mappedBy = "owner", fetch = FetchType.EAGER, cascade = CascadeType.ALL)
 	private Set<Cylinder> cylinders;
 
+	/**
+	 * The email address of this member
+	 */
 	@NotNull
 	private String email;
 
+	/**
+	 * The crypted password
+	 */
 	@NotNull
 	private String encodedPassword;
 
+	/**
+	 * Well... The members first name.
+	 */
 	@NotNull
 	@Size(min = 1, max = 25)
 	@Pattern(regexp = "[A-Za-z -]*", message = "must contain only letters and spaces")
 	private String firstName;
 
+	/**
+	 * DB generated uuid
+	 */
 	@Id
 	@GeneratedValue(generator = "increment")
 	@GenericGenerator(name = "increment", strategy = "increment")
 	private Long id;
 
+	/**
+	 * Flag indicating if the person is still an active club member
+	 */
 	private Boolean isActive = true;
 
+	/**
+	 * Flag indicating if the user has a gas blenber brevet and is allowed to
+	 * fill with the partitial blending method in general.
+	 */
 	private Boolean hasGasblenderBrevet = false;
 
+	/**
+	 * You might have guessed it already: This is the members last name. :-)
+	 */
 	@NotNull
 	@Size(min = 1, max = 25)
 	@Pattern(regexp = "[A-Za-z -]*", message = "must contain only letters and spaces")
 	private String lastName;
 
+	/**
+	 * The invoice will be sent every year or at a fixed amount of debt
+	 * configured global. If a member wish, he can define a personal maximum
+	 * debt amount lower than the global one.
+	 */
 	private int maximumDebit;
 
+	/**
+	 * Clubs member number
+	 */
 	@NotNull
 	private int memberNumber;
 
+	/**
+	 * The user hasn't changed the default password so far.
+	 */
 	private Boolean useDefaultPassword = true;
 
+	/**
+	 * Defines which rights the user has. Depends no last briefing and on users
+	 * brevets.
+	 */
 	@Embedded
 	private UserRights rights;
 
